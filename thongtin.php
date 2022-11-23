418cea9d130f661ad79f66ec8f3b633a2d4ddf72
Đức Huy <quysunho123@gmail.com>
2022-11-23 20:03:20 +0700

    change image url , admin search cart

--- thongtin.php
+++ thongtin.php
@@ -1,45 +1,43 @@
 <?php
-    include "./user/header.php"
+include "./user/header.php"
 ?>
-
-    
-    <!-- Navbar Start -->
-    <nav class="navbar navbar-expand-lg bg-white navbar-light shadow-sm py-3 py-lg-0 px-3 px-lg-0">
-        <a href="index.html" class="navbar-brand ms-lg-5">
-            <h1 class="m-0 text-uppercase text-dark">
-                <img src="./images/logo_vinpearl.jpg" width="60" height="60"/>
-                Vinpearl
-            </h1>
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <div class="navbar-nav ms-auto py-0">
-                <a href="index.php" class="nav-item nav-link">Trang chủ</a>
-                <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Loại Dịch Vụ</a>
-                    <div class="dropdown-menu m-0">
-                        <a href="nghiduong.php" class="dropdown-item">Nghỉ Dưỡng</a>
-                        <a href="khampha.php" class="dropdown-item">Khám phá & Hoạt động</a>
-                        <a href="golf.php" class="dropdown-item">Golf</a>
-                        <a href="amthuc.php" class="dropdown-item">Ẩm thực</a>
-                    </div>
+<!-- Navbar Start -->
+<nav class="navbar navbar-expand-lg bg-white navbar-light shadow-sm py-3 py-lg-0 px-3 px-lg-0">
+    <a href="index.html" class="navbar-brand ms-lg-5">
+        <h1 class="m-0 text-uppercase text-dark">
+            <img src="./images/logo_vinpearl.jpg" width="60" height="60" />
+            Vinpearl
+        </h1>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav ms-auto py-0">
+            <a href="index.php" class="nav-item nav-link">Trang chủ</a>
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Loại Dịch Vụ</a>
+                <div class="dropdown-menu m-0">
+                    <a href="nghiduong.php" class="dropdown-item">Nghỉ Dưỡng</a>
+                    <a href="khampha.php" class="dropdown-item">Khám phá & Hoạt động</a>
+                    <a href="golf.php" class="dropdown-item">Golf</a>
+                    <a href="amthuc.php" class="dropdown-item">Ẩm thực</a>
                 </div>
-                <a href="baitap.php" class="nav-item nav-link">Bài tập</a>
-                <a href="thongtin.php" class="nav-item nav-link active ">Thông tin</a>
-                <a href="#" class="nav-item nav-link">Admin</a>
-                
-                <a href="giohang.php" class="nav-item nav-link nav-contact bg-primary text-white px-5 ms-lg-5"><i class="bi bi-cart4"style="font-size:18px;"></i></a>
             </div>
+            <a href="baitap.php" class="nav-item nav-link">Bài tập</a>
+            <a href="thongtin.php" class="nav-item nav-link active ">Thông tin</a>
+            <a href="#" class="nav-item nav-link">Admin</a>
+            <a href="giohang.php" class="nav-item nav-link nav-contact bg-primary text-white px-5 ms-lg-5"><i class="bi bi-cart4" style="font-size:18px;"></i></a>
         </div>
-    </nav>
-    <!-- Navbar End -->
+    </div>
+</nav>
+<!-- Navbar End -->
 
-    <!-- Hero Start -->
-    alaolo
-    <!-- Hero End -->
+<!-- Hero Start -->
+
+    alo alo
+<!-- Hero End -->
 
 <?php
-    include "./user/footer.php"
+include "./user/footer.php"
 ?>
